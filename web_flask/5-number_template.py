e905568447481ae5dd68e94cceb2e4c3f6f415d1
dblay112 <brightdjogatse@gmail.com>
2024-01-02 06:20:53 -0800

    fixed pycodestyle errors

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -47,10 +47,11 @@ def n_number(n):
     return "{} is a number".format(n)
 
 
+
 @app.route("/number_template/<int:n>", strict_slashes=False)
-def n_template(n):
-    """Displays an HTML page only if <n> is an integer"""
-    return render_template('5-number_template.html', n=n)
+def number_template(n):
+    """Displays an HTML page only if <n> is an integer."""
+    return render_template("5-number.html", n=n)
 
 
 if __name__ == "__main__":
